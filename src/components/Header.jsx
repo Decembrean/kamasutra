e436ab9dc393eba9aad6fb40c495c6c2fe1d4d3f
Decembrean <rita-ast@mail.ru>
2021-10-16 23:29:34 +0300

    first commit

---
+++ src/components/Header.jsx
@@ -0,0 +1,9 @@
+import React from 'react';
+
+const Header = () => {
+    return <header className='header'>
+        <img className='pic' src='https://e7.pngegg.com/pngimages/174/24/png-clipart-green-leaves-illustration-leaf-logo-spring-green-leaf-logo-design-leaf-vegetable-other.png'/>
+    </header>
+}
+
+export default Header
---
+++ src/components/Profile.jsx
@@ -0,0 +1,24 @@
+import React from 'react';
+
+const Profile = () => {
+    return <div className='content'>
+        <div>
+            <img src="https://foto.co.id/wp-content/uploads/2017/08/ireland2.jpg" alt="foto"/>
+        </div>
+        <div>
+            ava + description
+        </div>
+        <div>
+            my posts
+            <div>
+                new post
+            </div>
+            <div>
+                <div>post1</div>
+                <div>post2</div>
+            </div>
+        </div>
+    </div>
+}
+
+export default Profile
